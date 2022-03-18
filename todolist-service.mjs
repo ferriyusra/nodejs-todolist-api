ad2412dd96801f2d303aed8cd2d0719821d7b8c6
ferriyusra <ferisakuragi@gmail.com>
2022-03-18 17:52:24 +0700

    nodejs todolist api no database

---
+++ todolist-service.mjs
@@ -0,0 +1,67 @@
+
+export class TodolistService {
+
+    todolist = [
+        "test todo 1",
+        "test todo 2",
+        "test todo 3",
+    ];
+
+    getJsonTodolist(){
+        return JSON.stringify({
+            code: 200,
+            status: "OK",
+            data: this.todolist.map((value,index) => {
+                return {
+                    id: index,
+                    todo: value
+                }
+            })
+        });
+    }
+
+    getTodolist(request,response) {
+
+
+        response.write(this.getJsonTodolist());
+        response.end();
+
+    }
+
+    createTodolist(request,response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            this.todolist.push(body.todo);
+
+            response.write(this.getJsonTodolist());
+            response.end();
+        });
+    }
+
+    updateTodolist(request,response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+
+            if (this.todolist[body.id]){
+                this.todolist[body.id] = body.todo
+            }
+
+            response.write(this.getJsonTodolist());
+            response.end();
+        });
+    }
+
+    deleteTodolist(request,response){
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+
+            if (this.todolist[body.id]){
+                this.todolist.splice(body.id, 1);
+            }
+
+            response.write(this.getJsonTodolist());
+            response.end();
+        });
+    }
+
+}
